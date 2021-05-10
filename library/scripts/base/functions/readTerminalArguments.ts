f89b5e1691f9fe4215af16f6f9e8eedb2868cf8e
Kok Kek <kokkekpek@gmail.com>
2021-05-11 02:07:53 +0300

    Replace console.log to console.error

--- library/scripts/base/functions/readTerminalArguments.ts
+++ library/scripts/base/functions/readTerminalArguments.ts
@@ -11,7 +11,7 @@ export default function(argumentsList: string[]): TerminalArgumentsInterface {
         console.error(`${colors.red('ERROR')}: Invalid arguments count.`)
         console.error(`Arguments:`)
         for (let i: number = 0; i < argumentsList.length; i++)
-            console.log(`    ${colors.yellow(argumentsList[i])}`)
+            console.error(`    ${colors.yellow(argumentsList[i])}`)
     }
     else {
         for (let i: number = 0; i < argumentsList.length; i++)
