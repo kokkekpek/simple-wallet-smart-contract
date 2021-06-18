5a096d76ad65d5513c5c5eca85287265c4bc00e0
Kok Kek <kokkekpek@gmail.com>
2021-06-18 05:08:01 +0300

    Rename constant.

--- scripts/wallet/deploy.ts
+++ scripts/wallet/deploy.ts
@@ -1,7 +1,7 @@
 import config from '../../configs/config'
 import SafeMultisigWalletDeployWithGiver from '../../library/scripts/deploy/samples/SafeMultisigWalletDeployWithGiver'
 
-const giverV2Deploy: SafeMultisigWalletDeployWithGiver = new SafeMultisigWalletDeployWithGiver({
+const deploy: SafeMultisigWalletDeployWithGiver = new SafeMultisigWalletDeployWithGiver({
     url: config.net.deploy.url,
     port: config.net.deploy.port,
     timeout: config.net.deploy.timeout,
@@ -11,4 +11,4 @@ const giverV2Deploy: SafeMultisigWalletDeployWithGiver = new SafeMultisigWalletD
     keys: config.net.deploy.contracts.wallet.keys,
     requiredTons: config.net.deploy.contracts.wallet.requiredTons
 })
-giverV2Deploy.run().then()
+deploy.run().then()
