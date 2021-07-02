1cda788fc010353f13c7d2f3e49fbc5350293849
Kok Kek <kokkekpek@gmail.com>
2021-07-02 17:19:56 +0300

    Update config interface

--- library/scripts/info/interfaces/InfoConfigInterface.ts
+++ library/scripts/info/interfaces/InfoConfigInterface.ts
@@ -2,6 +2,6 @@ import NetConfigInterface from '../../../config/NetConfigInterface'
 
 export default interface InfoConfigInterface {
     net: NetConfigInterface
+    locale: string | undefined
     keys: string
-    locale: string
 }
